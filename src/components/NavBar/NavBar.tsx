a9505971f6d64b433b49817d51d7051ff697c007
michael <michaeleze3@gmail.com>
2024-02-01 10:39:55 +0100

    update navbar and readme

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,15 @@
 "use client"
 import type { NavBarProps } from '@/types/index';
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 
 const Modal = dynamic(() => import('@/components/Modal/Modal'))
 
+import { useState } from 'react';
+
 const NavBar: React.FC<NavBarProps> = ({ title, link }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const showModal = () => {
     const modal = document.getElementById("my_modal_3") as HTMLDialogElement;
     if (modal) {
@@ -13,19 +18,31 @@ const NavBar: React.FC<NavBarProps> = ({ title, link }) => {
   }
 
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
+    <section className="navbar sticky top-0 z-10">
+      <div className="flex-0">
         <a className="btn btn-ghost text-xl">{title}</a>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
+      <div className="p-4 flex md:rounded-full backdrop-blur-sm bg-black/30 flex-col md:flex-row gap-20 md:gap-0">
+        <div className={`${isOpen && 'border-b'}  py-3`}>
+          <button className="md:hidden text-white" onClick={() => setIsOpen(!isOpen)}>MENU</button>
+        </div>
+        <nav className={`flex flex-col md:flex-row items-center md:justify-between space-y-4 md:space-y-0 md:space-x-10 ${isOpen ? 'block' : 'hidden'} md:block max-w-full max-h-full`}>
+          <Link className="text-white hover:text-blue-500 transition-colors" href="#">
+            Home
+          </Link>
+          <Link className="text-white hover:text-blue-500 transition-colors" href="#">
+            About
+          </Link>
+          <Link className="text-white hover:text-blue-500 transition-colors" href="#">
+            Contact
+          </Link>
+          <Link className="text-white hover:text-blue-500 transition-colors" href="#">
             <button className="btn btn-primary place-content-center" onClick={showModal}>{link}</button>
-            <Modal subtitle='Personalize News Feed'/>
-          </li>
-        </ul>
-      </div>
-    </div>
+            <Modal subtitle='Personalize News Feed' />
+          </Link>
+        </nav>
+      </div >
+    </section>
   );
 };
 
